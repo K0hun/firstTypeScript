57870f4de6dafef618037cb2fd348826993e63cf
Hun <dudgns9194@gmail.com>
2024-06-17 15:01:16 +0900

    자바스크립트와 타입스크립트 비교

---
+++ a.ts
@@ -0,0 +1,14 @@
+
+// 자바스크립트
+function plus1(a, b){
+    return a+b;
+}
+
+console.log(plus1(3, 5));
+
+// 타입스크립트
+function plus2(a : number, b : number): number{
+    return a + b;
+}
+
+console.log(plus2(3, 5));
