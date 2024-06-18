ce1501c8149d036b0c9959a6d03b3219fdc4f6c5
Hun <dudgns9194@gmail.com>
2024-06-18 16:20:07 +0900

    getter와 setter 실습

--- oop.js
+++ oop.js
@@ -1,16 +1,27 @@
 var Employee = /** @class */ (function () {
-    function Employee(empName, age, empJob) {
+    function Employee(// 선언과 동시에 초기화
+    _empName, _age, _empJob) {
         var _this = this;
+        this._empName = _empName;
+        this._age = _age;
+        this._empJob = _empJob;
         this.printEmp = function () {
-            console.log("".concat(_this.empName, "\uC758 \uB098\uC774\uB294 ").concat(_this.age, "\uC774\uACE0, \uC9C1\uC5C5\uC740 ").concat(_this.empJob, "\uC785\uB2C8\uB2E4."));
+            console.log("".concat(_this._empName, "\uC758 \uB098\uC774\uB294 ").concat(_this._age, "\uC774\uACE0, \uC9C1\uC5C5\uC740 ").concat(_this._empJob, "\uC785\uB2C8\uB2E4."));
         };
-        this.empName = empName;
-        this.age = age;
-        this.empJob = empJob;
     }
+    Object.defineProperty(Employee.prototype, "empName", {
+        get: function () {
+            return this._empName;
+        },
+        set: function (val) {
+            this._empName = val;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Employee;
 }());
-;
-var employee1 = new Employee('kim', 20, '개발자');
-// employee1.empName = 'lee' // 접근지정 제한
+var employee1 = new Employee("kim", 20, "개발자");
+// employee1._empName = 'lee' // 접근지정 제한
+employee1.empName = "lee";
 employee1.printEmp();
--- oop.ts
+++ oop.ts
@@ -1,19 +1,27 @@
-class Employee{
-    private empName: string;
-    private age: number;
-    private empJob: string;
+class Employee {
 
-    constructor(empName: string, age: number, empJob: string){
-        this.empName = empName;
-        this.age = age;
-        this.empJob = empJob;
-    }
+  constructor( // 선언과 동시에 초기화
+    private _empName: string,
+    private _age: number,
+    private _empJob: string
+  ) {}
 
-    public printEmp = (): void => {
-        console.log(`${this.empName}의 나이는 ${this.age}이고, 직업은 ${this.empJob}입니다.`);
-    }
-};
+  public printEmp = (): void => {
+    console.log(
+      `${this._empName}의 나이는 ${this._age}이고, 직업은 ${this._empJob}입니다.`
+    );
+  };
 
-let employee1 = new Employee('kim', 20, '개발자');
-// employee1.empName = 'lee' // 접근지정 제한
+  public get empName() {
+    return this._empName;
+  }
+
+  public set empName(val: string) {
+    this._empName = val;
+  }
+}
+
+let employee1 = new Employee("kim", 20, "개발자");
+// employee1._empName = 'lee' // 접근지정 제한
+employee1.empName = "lee";
 employee1.printEmp();
